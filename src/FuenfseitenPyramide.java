d3cd2f9c9bdecc5a0b2422993ffa208ab9d425bd
gleni <glesel19@htl-shkoder.com>
2025-03-04 12:20:46 +0100

    FuenfseitePyramide commit 3

--- src/FuenfseitenPyramide.java
+++ src/FuenfseitenPyramide.java
@@ -9,17 +9,6 @@ public class FuenfseitenPyramide extends abstractPyramid {
     public float getSeitenlaenge() {
         return seitenlaenge;
     }
-
-    public double calcPerimeter() {
-        return 5 * seitenlaenge;
-    }
-
-    public double calcSyprine() {
-        double a = seitenlaenge;
-        double h = getHoehe();
-        double s = Math.sqrt((a / (2 * Math.tan(Math.toRadians(54)))) * (a / (2 * Math.tan(Math.toRadians(54)))) + h * h);
-        return (5 * a * s) / 2;
-    }
     @Override
     public double calcVolumen() {
         double a = seitenlaenge;
