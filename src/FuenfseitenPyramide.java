b414a307e4c2673d7d1e6da55932ffcf81768303
gleni <glesel19@htl-shkoder.com>
2025-03-04 12:02:47 +0100

    FuenfseitePyramide commit 2

--- src/FuenfseitenPyramide.java
+++ src/FuenfseitenPyramide.java
@@ -20,4 +20,11 @@ public class FuenfseitenPyramide extends abstractPyramid {
         double s = Math.sqrt((a / (2 * Math.tan(Math.toRadians(54)))) * (a / (2 * Math.tan(Math.toRadians(54)))) + h * h);
         return (5 * a * s) / 2;
     }
+    @Override
+    public double calcVolumen() {
+        double a = seitenlaenge;
+        double apothem = a / (2 * Math.tan(Math.toRadians(54)));
+        double grundflaeche = (5 * a * apothem) / 2;
+        return (grundflaeche * getHoehe()) / 3;
+    }
 }
