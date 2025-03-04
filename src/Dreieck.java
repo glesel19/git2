63b9229897b7508427f5264ea0a01eccb93e59c6
EneHax20 <EneHax20@htl-shkoder.com>
2025-03-04 12:04:41 +0100

    MA HE MA HE MA HE MA HE

--- src/Dreieck.java
+++ src/Dreieck.java
@@ -1,6 +1,12 @@
 public class Dreieck extends abstractPyramid {
+    float kantenlaenge;
 
     public Dreieck(float hoehe) {
         super(hoehe);
+        this.kantenlaenge = kantenlaenge;
     }
+
+
+
+
 }
